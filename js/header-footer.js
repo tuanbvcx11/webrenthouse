6bb11ff09984aab4819ecedcd10c7f2054b4ff64
Le Tuan <tuancules24@gmail.com>
2020-11-28 22:32:22 +0700

    header-footer

--- js/header-footer.js
+++ js/header-footer.js
@@ -5,4 +5,43 @@ $(document).ready(function() {
 		/* Act on the event */
 		$('.list-thongbao').fadeToggle(200);
 	});
+
+	//kiểm tra đăng nhập
+    $.ajax({
+        url : 'module/function/checkss-header.php',
+        type : 'post',
+        dataType : 'text',
+        data : {
+        },
+        success : function (result){
+            if (result == 'nologin') {
+            	$('.tuychon').css('display', 'none');
+            	$('.thongbao').css('display', 'none');
+            	$('.yeuthich').css('display', 'none');
+            	$('#hello').css('display', 'none');
+            	$('.dangnhap').css('display', 'block');
+            	$('.dangnhap').css('margin-right', '10px');
+            } else if (result == 'guest') {
+            	$('.dangnhap').css('display', 'none');
+            	// $('.tuychon').css('display', 'block');
+            	$('.thongbao').css('display', 'none');
+            	// $('.yeuthich').css('display', 'block');
+            	// $('#hello').css('display', 'block');
+            	$('.dangbai').css('display', 'none');
+            	$('.list-posted').css('display', 'none');
+            	$('.host-chat').css('display', 'none');
+            	$('.toadmin').css('display', 'none');
+            } else if (result == "host") {
+            	$('.toadmin').css('display', 'none');
+            	$('.dangnhap').css('display', 'none');
+            } else {
+            	$('.host-chat').css('display', 'none');
+            	$('.dangnhap').css('display', 'none');
+            	$('.info-me').css('display', 'none');
+            }
+        },
+        error : function (result) {
+            alert("lỗi");
+        }
+    });
 });
