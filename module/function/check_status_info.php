53b577572a1593b257bc436345eed8b27d2b3c0c
nguyenthu18012000 <57081407+nguyenthu18012000@users.noreply.github.com>
2020-12-20 13:51:09 +0700

    thu20.12

---
+++ module/function/check_status_info.php
@@ -0,0 +1,21 @@
+<?php  
+	// kiểm tra phiên đăng nhập cho phần header
+	session_start();
+	header('Content-Type: text/html; charset=UTF-8');
+    include_once("config.php");
+
+    $id = isset($_POST['id']) ? $_POST['id'] : 0;
+    $result = "other";
+
+    $stmt = $db->prepare("SELECT * FROM `user` WHERE id_user = ?");
+	$stmt->execute([$id]);
+	$count = $stmt->rowCount();
+	if ($count > 0){
+		$row = $stmt->fetch();
+		if ($row['status'] == "0"){
+			$result = "chuaduyet";
+		}
+	}
+
+	echo $result;
+?>
